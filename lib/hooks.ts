1814c5edd246d06d2b547275990888c654aa785b
franco <ougaoyuan0718@gmail.com>
2024-07-13 14:15:47 +1000

    url corresponds to scrolling

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,6 +1,7 @@
 import { useInView } from "react-intersection-observer";
 import { useActiveSectionContext } from "../context/active-section-context";
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 
 export function useSectionInView({
@@ -15,13 +16,17 @@ export function useSectionInView({
       initialInView: false,
       threshold: threshold
     });
+    const router = useRouter();
   
     useEffect(()=>{
         if (inView && activeSection === 'Home' && initialLoad){
             setInitialLoad(false);
+            router.push('/#home');
         }
         else if (inView && Date.now() - timeOfLastClick > 1000){
             setActiveSection(sectionName);
+            router.push('/#'+sectionName.toLowerCase());
+
         }
        
     }, [inView, setActiveSection, timeOfLastClick, sectionName])
