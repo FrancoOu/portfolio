a9e6d930d6d98e7b76281d7ff208eb615c28b7dd
franco <ougaoyuan0718@gmail.com>
2024-07-13 22:29:16 +1000

    fix scrolling issue on mobile

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,7 +1,6 @@
 import { useInView } from "react-intersection-observer";
 import { useActiveSectionContext } from "../context/active-section-context";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 
 export function useSectionInView({
@@ -16,16 +15,17 @@ export function useSectionInView({
       initialInView: false,
       threshold: threshold
     });
-    const router = useRouter();
   
     useEffect(()=>{
         if (inView && activeSection === 'Home' && initialLoad){
             setInitialLoad(false);
-            router.push('/#home');
+            history.pushState({}, '', '#home')
+            // router.push('/#home');
         }
         else if (inView && Date.now() - timeOfLastClick > 1000){
             setActiveSection(sectionName);
-            router.push('/#'+sectionName.toLowerCase());
+            history.pushState({}, '', '#'+sectionName.toLowerCase())
+            // router.push('/#'+sectionName.toLowerCase());
 
         }
        
